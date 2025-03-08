bd862745431fbaee67283292f15f7c74cd135bb8
Gustavo Santos <devgustavosantos@gmail.com>
2025-03-08 11:46:23 -0300

    feat(Header): adds a modal for new transactions

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { mainLogo } from '@/assets';
+import { Modal, NewTransaction } from '@/components';
 import { DICTIONARY } from '@/utils';
 
 import * as S from './Header.styles';
@@ -11,9 +12,15 @@ export function Header() {
           src={mainLogo}
           alt=""
         />
-        <S.NewTransactionButton variant="medium">
-          {DICTIONARY.NEW_TRANSACTION_BUTTON}
-        </S.NewTransactionButton>
+        <Modal
+          content={<NewTransaction />}
+          title={DICTIONARY.NEW_TRANSACTION}
+          trigger={
+            <S.NewTransactionButton variant="medium">
+              {DICTIONARY.NEW_TRANSACTION}
+            </S.NewTransactionButton>
+          }
+        />
       </S.HeaderWrapper>
     </S.HeaderContainer>
   );
