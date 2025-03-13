b77e6700476cc73882a28126025ca1ae2fae0dcb
Gustavo Santos <devgustavosantos@gmail.com>
2025-03-13 08:20:43 -0300

    refactor(modal): abstracts the styles
    this will allow them to be used in other types of modals

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,3 +1,4 @@
 export { BREAKPOINTS } from './breakpoints';
 export { COLORS } from './colors';
+export { modal } from './modal';
 export { Styling } from './Styling';
---
+++ src/styles/modal.ts
@@ -0,0 +1,48 @@
+import { css } from 'styled-components';
+
+import { BREAKPOINTS } from './breakpoints';
+
+const overlay = css`
+  position: fixed;
+  inset: 0;
+  background-color: rgba(0, 0, 0, 75%);
+  z-index: 999;
+`;
+
+const content = css`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  background-color: ${({ theme }) => theme.BASE_800};
+  z-index: 9999;
+  padding: 48rem;
+  border-radius: 6rem;
+  box-shadow: 0 4rem 32rem rgba(0, 0, 0, 80%);
+
+  @media only screen and (max-width: ${BREAKPOINTS.SMALL}) {
+    max-width: initial;
+    transform: initial;
+    inset: auto 0 0 0;
+    padding: 24rem;
+    border-radius: 6rem 6rem 0 0;
+  }
+`;
+
+const title = css`
+  font-weight: 700;
+  font-size: 24rem;
+  line-height: 160%;
+  color: ${({ theme }) => theme.BASE_300};
+
+  @media only screen and (max-width: ${BREAKPOINTS.SMALL}) {
+    font-size: 20rem;
+  }
+`;
+
+export const modal = {
+  overlay,
+  content,
+  title,
+};
