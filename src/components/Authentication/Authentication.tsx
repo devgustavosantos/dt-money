49a063ce1304361115049d5e702ea34778918826
Gustavo Santos <devgustavosantos@gmail.com>
2025-05-23 07:25:10 -0300

    fix(authentication): comparison with wrong values

--- src/components/Authentication/Authentication.tsx
+++ src/components/Authentication/Authentication.tsx
@@ -1,10 +1,11 @@
 import { SignIn, LinkSent } from '@/components';
 import { useAuthenticationContext } from '@/contexts';
+import { AllowedSteps } from '@/types';
 
 export function Authentication() {
   const { currentStep } = useAuthenticationContext();
 
-  if (currentStep === 'send') return <LinkSent />;
+  if (currentStep === AllowedSteps.SEND) return <LinkSent />;
 
   return <SignIn />;
 }
