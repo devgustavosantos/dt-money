aa3416344ad8643cc1b83e54ec7f4c7404182bd9
Gustavo Santos <devgustavosantos@gmail.com>
2025-05-24 09:46:46 -0300

    feat: adds the flow to confirm and finalize the login

--- src/contexts/Authentication/Authentication.provider.tsx
+++ src/contexts/Authentication/Authentication.provider.tsx
@@ -1,29 +1,11 @@
-import { useState } from 'react';
-
-import { AllowedSteps } from '@/types';
-
-import { AuthenticationProviderProps, StepType } from './Authentication.types';
-import { CURRENT_STEP_KEY } from './Authentication.utils';
+import { useAuthentication } from './Authentication.hook';
+import { AuthenticationProviderProps } from './Authentication.types';
 import { AuthenticationContext } from './index';
 
 export function AuthenticationProvider({
   children,
 }: AuthenticationProviderProps) {
-  const [currentStep, setCurrentStep] = useState<StepType>(() => {
-    const stepInStorage = window.localStorage.getItem(CURRENT_STEP_KEY);
-
-    const stepFound = Object.values(AllowedSteps).find(
-      (step) => step === stepInStorage,
-    );
-
-    return stepFound ? stepFound : AllowedSteps.WELCOME;
-  });
-
-  function handleCurrentStep(step: StepType) {
-    setCurrentStep(step);
-
-    window.localStorage.setItem(CURRENT_STEP_KEY, step);
-  }
+  const { currentStep, handleCurrentStep } = useAuthentication();
 
   return (
     <AuthenticationContext.Provider value={{ currentStep, handleCurrentStep }}>
