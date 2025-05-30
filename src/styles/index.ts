d43ba061da552d58023d3ffab74aac55a23a8cb5
Gustavo Santos <devgustavosantos@gmail.com>
2025-05-30 08:50:59 -0300

    feat(styles): adds reusable styled components

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -2,3 +2,5 @@ export { BREAKPOINTS } from './breakpoints';
 export { COLORS } from './colors';
 export { modal, custom } from './modal';
 export { Styling } from './Styling';
+export { Spinner } from './spinner';
+export { Error } from './error';
